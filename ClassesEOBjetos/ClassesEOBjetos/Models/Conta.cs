7f72fa527195006c471627fbf7ef8ed6e6264013
Eduardo Assis da Silva <eduardo.silva@lemaf.ufla.br>
2015-05-05 17:17:56 -0300

    Criação do projeto para trabalhar com classes e objetos

---
+++ ClassesEOBjetos/ClassesEOBjetos/Models/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesEOBjetos.Models
+{
+    class Conta
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+    }
+}
